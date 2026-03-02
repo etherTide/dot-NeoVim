466ddde477c04a0613d0f94410a60e3e29321e50
Ether <134476332+etherTide@users.noreply.github.com>
2026-03-03 00:49:45 +1100

    Colourscheme now loads on startup

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,4 +4,10 @@ return {
   lazy = false,
   priority = 1000,
   opts = {},
+  -- config is required so that vim.cmd can
+  -- be called to activate installed colourscheme
+  config = function()
+    require("tokyonight").setup(opts)
+    vim.cmd([[colorscheme tokyonight-moon]])
+  end
 }
